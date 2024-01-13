ba40122c940b42261fbb1c85188a738aa4489eea
nischaygadher15 <nischaygadher15@gmail.com>
2024-01-14 03:35:05 +0530

    Citizen Solar > Home Page > Animated Counter Finished.

--- CitizenSolar/Js/script.js
+++ CitizenSolar/Js/script.js
@@ -30,6 +30,7 @@ function hideCross()
 
 
 //Sticky Nav Bar After Scroll Pass Header Section
+
 const lowerHeadInt = document.querySelector(".intersection");
 const navBar = document.querySelector(".stickyNavbar");
 const observer = new IntersectionObserver(changeNavbar,
@@ -56,6 +57,103 @@ observer.observe(lowerHeadInt);
 
 
 //Animated Up Counting Numnbers
+
+const counterTgr1 = document.querySelector("#citizenSolar");
+const observer1 = new IntersectionObserver(triggerF,
+    {
+        root:null,
+        rootMargin: "50px",
+        threshold: 1
+    });
+
+observer1.observe(counterTgr1);
+
+
+const counterTgr2 = document.querySelector("#countingNum");
+const observer2 = new IntersectionObserver(triggerT,
+    {
+        root:null,
+        rootMargin: "0px",
+        threshold: 0.8
+    });
+
+observer2.observe(counterTgr2);
+
+
+function triggerF(entries)
+{
+    let ent1 = entries[0];
+    if (ent1.isIntersecting == false)
+    {
+        console.log("ent1:",ent1.isIntersecting);
+        startCounter();
+    }
+}
+
+function triggerT(entries)
+{
+    let ent2 = entries[0];
+    if (ent2.isIntersecting == true)
+    {
+        console.log("ent2:",ent2.isIntersecting);
+        startCounter();
+    }
+}
+
+
+function startCounter()
+{
+    let countArray = document.querySelectorAll('.counterNum');
+    let speed = 40;
+
+    countArray.forEach( (countEle) => {
+        let target = parseInt(countEle.getAttribute('data-set'));
+        let initialValue = 0; 
+        let incVar = target / speed;
+
+        function updateCounter()
+        {
+            initialValue += incVar;
+            countEle.innerText = Math.floor(initialValue)
+
+            if(initialValue < target)
+            {
+                setTimeout(() => {updateCounter()},100);
+            }
+            else{
+                if (countEle.innerText == 180)
+                {
+                countEle.innerText += "MW";
+                }
+                else
+                {
+                countEle.innerText += "+";
+                }
+
+            }
+        }
+        updateCounter();
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
